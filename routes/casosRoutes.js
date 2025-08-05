331ac51cd254e7906055937a6c2f9fc86a915a1f
Matheus N <matheusnazareth21@gmail.com>
2025-08-05 18:18:33 -0300

    Teste 9

--- routes/casosRoutes.js
+++ routes/casosRoutes.js
@@ -6,7 +6,6 @@ router.get('/', Casoscontroller.getAll);
 router.get('/:id', Casoscontroller.getById);
 router.post('/', Casoscontroller.create);
 router.put('/:id', Casoscontroller.update);
-router.patch('/:id', Casoscontroller.partialUpdate);
 router.delete('/:id', Casoscontroller.remove);
 
 module.exports = router;
